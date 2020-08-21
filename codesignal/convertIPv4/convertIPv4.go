0ec67deff6d7d79c13d76cd19ebb56449b0bf0cb
maxcian <max.cian@cellopoint.com>
2020-08-21 14:12:16 +0800

    ✨ Feat: (codesignal) Add convertIPv4 solution: convertion of IPv6

--- codesignal/convertIPv4/convertIPv4.go
+++ codesignal/convertIPv4/convertIPv4.go
@@ -1,33 +1,62 @@
 package convertipv4
 
 import (
+	"fmt"
 	"math"
 	"strconv"
 	"strings"
 )
 
-func equivalentInterger(ipv4 string) string {
+func toHex(n int64) string {
+	return strconv.FormatInt(n, 16)
+}
+
+func ipv4ToHex(nums []int64) string {
+	// Hint from: https://www.researchgate.net/figure/IPv4-to-IPv6-Conversion-Method1-In-this-method-firstly-to-convert-the-Decimal-IPv4_fig1_271294793
+	// Details: https://zh.wikipedia.org/wiki/IPv6
+	hexValues := make([]string, 0, 8)
+	for _, n := range nums {
+		leftBits := n >> 4
+		rightBits := n & 0x0F
+		hexValues = append(hexValues, toHex(leftBits), toHex(rightBits))
+	}
+	return fmt.Sprintf("0:0:0:0:0:ffff:%v:%v", strings.Join(hexValues[:4], ""), strings.Join(hexValues[4:], ""))
+}
+
+func equivalentInterger(nums []int64) string {
 	// Useful tool:
 	// 		http://www.aboutmyip.com/AboutMyXApp/IP2Integer.jsp?ipAddress=216.58.200.14
 	// golang int/string convert hints:
 	// 		https://yourbasic.org/golang/convert-int-to-string/
 	//
-	values := strings.Split(ipv4, ".")
-	// fmt.Println(values)
 	var ret float64 = 0
-	for i, s := range values {
-		if n, err := strconv.Atoi(s); err == nil {
-			// [216 58 200 14]
-			// [11011000, 00111010, 11001000, 00001110]
-			// [ 2^(8*3),  2^(8*2),  2^(8*1),  2^(8*0) ]
-			multiplier := math.Pow(float64(2), float64((len(values)-i-1)*8))
-			ret += float64(multiplier) * float64(n)
-		}
+	for i, n := range nums {
+		// [216 58 200 14]
+		// [11011000, 00111010, 11001000, 00001110]
+		// [ 2^(8*3),  2^(8*2),  2^(8*1),  2^(8*0) ]
+		multiplier := math.Pow(float64(2), float64((len(nums)-i-1)*8))
+		ret += float64(multiplier) * float64(n)
 	}
 	return strconv.Itoa(int(ret))
 }
 
 func convertIPv4(ipv4 string) []string {
-	eq := equivalentInterger(ipv4)
-	return []string{eq, "not"}
+	values := strings.Split(ipv4, ".")
+	if len(values) != 4 {
+		return []string{}
+	}
+	nums := make([]int64, 0, 4)
+	for _, s := range values {
+		if n, err := strconv.Atoi(s); err == nil {
+			if n > 255 || n < 0 {
+				return []string{}
+			}
+			nums = append(nums, int64(n))
+		} else {
+			return []string{}
+		}
+	}
+	eqInt := equivalentInterger(nums)
+	ipv6 := ipv4ToHex(nums)
+	return []string{eqInt, ipv6}
 }
--- codesignal/convertIPv4/convertIPv4_test.go
+++ codesignal/convertIPv4/convertIPv4_test.go
@@ -11,7 +11,11 @@ func Test_convertipv4(t *testing.T) {
 		input  string
 		expect []string
 	}{
-		{"Example", "216.58.200.14", []string{"3627730958", "not"}},
+		{"Example", "216.58.200.14", []string{"3627730958", "0:0:0:0:0:ffff:d83a:c80e"}},
+		{"Test 2", "2.2.2", []string{}},
+		{"Guess1", "256.255.255.255", []string{}},
+		{"Guess2", "-1.255.255.255", []string{}},
+		{"Guess3", ".255.255.255", []string{}},
 	}
 	for _, test := range tests {
 		got := convertIPv4(test.input)
