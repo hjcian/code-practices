a1490fd58cdf38732a362a271f08c49a90e12e58
hjcian <chutarojian@gmail.com>
2020-08-27 21:25:07 +0800

    🚀 Perf: Update #746: add constant space solution

--- leetcode/problems/0746.MinCostClimbingStairs/0746.MinCostClimbingStairs.go
+++ leetcode/problems/0746.MinCostClimbingStairs/0746.MinCostClimbingStairs.go
@@ -11,11 +11,26 @@ func min(a, b int) int {
 	return b
 }
 
+// func minCostClimbingStairs(cost []int) int {
+// 	minCost := make([]int, len(cost), len(cost))
+// 	minCost[0], minCost[1] = cost[0], cost[1]
+// 	for i := 2; i < len(cost); i++ {
+// 		minCost[i] = cost[i] + min(minCost[i-1], minCost[i-2])
+// 	}
+// 	return min(minCost[len(minCost)-1], minCost[len(minCost)-2])
+// }
+
+// 其實只需要常數空間，紀錄前兩個最優解
+// [a, b, c, d, e]
+//	last1=a
+//  last2=b
+// 在 c 這步，我只需要知道我是 last1+c 比較優、還是 last2+c 比較優
+// 比較優的解存入 last2；原本的 last2 存入 last1
+
 func minCostClimbingStairs(cost []int) int {
-	minCost := make([]int, len(cost), len(cost))
-	minCost[0], minCost[1] = cost[0], cost[1]
+	last1, last2 := cost[0], cost[1]
 	for i := 2; i < len(cost); i++ {
-		minCost[i] = cost[i] + min(minCost[i-1], minCost[i-2])
+		last1, last2 = last2, min(cost[i]+last1, cost[i]+last2)
 	}
-	return min(minCost[len(minCost)-1], minCost[len(minCost)-2])
+	return min(last1, last2)
 }
