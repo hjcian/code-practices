333c445cad6d2238ca2dab18b93bd6ca482fc301
hjcian <chutarojian@gmail.com>
2020-10-01 14:54:41 +0800

    ✨ Feat: Add composeRanges and its test

---
+++ codesignal/composeRanges/composeRanges.js
@@ -0,0 +1,21 @@
+function composeRanges (nums) {
+  const ret = []
+  if (nums.length !== 0) {
+    let start = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] !== nums[i - 1] + 1) {
+        const end = nums[i - 1]
+        ret.push(start !== end ? `${start}->${end}` : `${end}`)
+
+        start = nums[i]
+      }
+    }
+
+    const end = nums[nums.length - 1]
+    ret.push(start !== end ? `${start}->${end}` : `${end}`)
+  }
+  return ret
+}
+
+module.exports = composeRanges
---
+++ codesignal/composeRanges/composeRanges.test.js
@@ -0,0 +1,42 @@
+const _ = require('lodash')
+const assert = require('assert')
+const { expect } = require('chai')
+
+const composeRanges = require('./composeRanges')
+
+describe('Test', () => {
+  const tests = [
+    {
+      name: 'Example',
+      nums: [-1, 0, 1, 2, 6, 7, 9],
+      want: ['-1->2', '6->7', '9']
+    },
+    {
+      name: 'Test 2',
+      nums: [],
+      want: []
+    },
+    {
+      name: 'Test 11',
+      nums: [0, 5, 9],
+      want: ['0', '5', '9']
+    },
+    {
+      name: 'Test 13',
+      nums: [7, 8, 9],
+      want: ['7->9']
+    },
+    {
+      name: 'Test 14',
+      nums: [0, 1, 9],
+      want: ['0->1', '9']
+    }
+  ]
+
+  tests.forEach(test => {
+    it(test.name, () => {
+      const expectation = expect(composeRanges(test.nums))
+      expectation.to.deep.equal(test.want)
+    })
+  })
+})
