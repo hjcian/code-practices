4438b48c84bfd7eadb9795b16e6ed1172bea8fde
hjcian <chutarojian@gmail.com>
2020-10-06 21:12:15 +0800

    🔨 Refactor: Update composeRanges

--- codesignal/composeRanges/composeRanges.js
+++ codesignal/composeRanges/composeRanges.js
@@ -1,20 +1,21 @@
 function composeRanges (nums) {
+  if (nums.length === 0) { return [] }
+
   const ret = []
-  if (nums.length !== 0) {
-    let start = nums[0]
+  let start = nums[0]
 
-    for (let i = 1; i < nums.length; i++) {
-      if (nums[i] !== nums[i - 1] + 1) {
-        const end = nums[i - 1]
-        ret.push(start === end ? `${end}` : `${start}->${end}`)
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] !== 1) {
+      const end = nums[i - 1]
+      ret.push(start === end ? `${end}` : `${start}->${end}`)
 
-        start = nums[i]
-      }
+      start = nums[i]
     }
-
-    const end = nums[nums.length - 1]
-    ret.push(start === end ? `${end}` : `${start}->${end}`)
   }
+
+  const end = nums[nums.length - 1]
+  ret.push(start === end ? `${end}` : `${start}->${end}`)
+
   return ret
 }
 
