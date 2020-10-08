648376ab242b7488e2c5cbb9f05452f537a59132
hjcian <chutarojian@gmail.com>
2020-10-08 23:49:23 +0800

    🚀 Perf: Add benchmark to compare the 're' solution and string manipulation

--- codewars/Returnsubstringinstancecount2/Returnsubstringinstancecount2.py
+++ codewars/Returnsubstringinstancecount2/Returnsubstringinstancecount2.py
@@ -4,20 +4,20 @@ def search_substr(full_text, search_text, allow_overlap=True):
     if not full_text or not search_text:
         return 0
 
+    # these init. can reduce excution time during while loop
+    limit = len(full_text) 
+    step = allow_overlap and 1 or len(search_text)
+    
     start_pos = 0
     count = 0
-    while start_pos < len(full_text):
+    while start_pos < limit:
         matched_pos = full_text.find(search_text, start_pos)
 
-        if matched_pos == -1: break
-        else:
-            count += 1
-            start_pos = matched_pos
-            if allow_overlap:
-                start_pos += 1
-            else:
-                start_pos += len(search_text)
-    return count
+        if matched_pos == -1: 
+            return count # early return to reduce instruction
+        
+        count += 1
+        start_pos = matched_pos + step
 
 class MimicTestObject(object): pass
 
@@ -41,5 +41,28 @@ class TestObject(unittest.TestCase):
         Test.assert_equals(search_substr('', ''), 0)
         Test.assert_equals(search_substr('', '',False), 0)
 
+import re
+def re_search_substr(full_text, search_text, allow_overlap=True):
+    if not (full_text and search_text): return 0
+    return len(re.findall(f"({allow_overlap and '?=' or ''}{search_text})", full_text))
+    
+
+def benchmark():
+    import timeit
+
+    template = '''
+{0}('aa_bb_cc_dd_bb_e', 'bb')
+{0}('aaacccbbbcccc', 'cc')
+{0}('aaabbbaaa', 'bb',False)
+'''
+
+    print("Start C style (string manipulation)")
+    res = timeit.timeit(template.format("search_substr"), setup="from __main__ import search_substr")
+    print(f"{res:.5f} s")
+    print("Start Pythonic style (leverage re module)")
+    res = timeit.timeit(template.format("re_search_substr"), setup="from __main__ import re_search_substr")
+    print(f"{res:.5f} s")
+
 if __name__ == '__main__':
+    benchmark()
     unittest.main()
