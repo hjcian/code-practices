32ba4e2d737e5779e156b756bfb90fdb725d54a4
maxcian <max.cian@cellopoint.com>
2020-10-21 09:57:37 +0800

    📚 Docs: Add notes to add_overflow_detect

--- interesting-problems/bitwise-ops-addOk/add_overflow_detect.go
+++ interesting-problems/bitwise-ops-addOk/add_overflow_detect.go
@@ -1,41 +1,68 @@
 package addoverflowdetect
 
-import (
-	"fmt"
-)
+// Truth table of Signed Bits of Two Integers
+//          +   |   -
+//      |   000 |   111
+//  ----------------------
+// +    |   000 |   000    (& and)
+//  000 |   000 |   111    (| or )
+//      |   000 |   111    (^ xor)
+//  ----------------------
+// -    |   000 |   111
+//  111 |   111 |   111
+//      |   111 |   000
 
-const MaxUint8 = ^uint8(0)
-const MinUint8 = 0
-const MaxInt8 = int8(MaxUint8 >> 1)
-const MinInt8 = -MaxInt8 - 1
-
-func abs8(n int8) int8 {
-	mask := n >> 7
-	return (n ^ mask) - mask
-}
-
-func printBin8(n int8) {
-	fmt.Printf("%08b | %v \n", uint8(n), n)
-}
+// Consider
+//         +               |  -
+//   +  | (a+b)>>n == 000  |  (always OK)
+//   -  | (always OK)      |  (a+b)>>n == 111
 
 func addOK(a, b int8) bool {
-	maskA := a >> 7
-	maskB := b >> 7
-	compareTarget := maskA & maskB
-	mask := int8(uint8(maskA^maskB) ^ 0xFF)
-	// printBin8(int8(magic))
-	// fmt.Println("a, b, magic:", a, b, magic)
+	aSignBits := a >> 7
+	bSignBits := b >> 7
+	compareTarget := aSignBits & bSignBits
+	mask := int8(uint8(aSignBits^bSignBits) ^ 0xFF)
 	return ((a+b)>>7)&mask == compareTarget
 }
+
+// Given:
+//     MinInt = MaxInt + 1
+
+// Truth table of Signed Bits of Two Integers
+//          +   |   -
+//      |   000 |   111
+//  ----------------------
+// +    |   000 |   000    (& and)
+//  000 |   000 |   111    (| or )
+//      |   000 |   111    (^ xor)
+//  ----------------------
+// -    |   000 |   111
+//  111 |   111 |   111
+//      |   111 |   000
+
+// Consider:
+//                +         |       -
+//     +    a + b < MaxInt  |  (always OK)
+//     -    (always OK)     |  a + b > MinInt = MaxInt + 1
+
+// const MaxUint8 = ^uint8(0)
+// const MinUint8 = 0
+// const MaxInt8 = int8(MaxUint8 >> 1)
+// const MinInt8 = -MaxInt8 - 1
+
+// func abs8(n int8) int8 {
+// 	mask := n >> 7
+// 	return (n ^ mask) - mask
+// }
+
+// func printBin8(n int8) {
+// 	fmt.Printf("%08b | %v \n", uint8(n), n)
+// }
 
 // func addOK(a, b int8) bool {
 // 	maskA := a >> 7
 // 	maskB := b >> 7
 // 	mask := maskA & maskB
-
 // 	magic := uint8(maskA^maskB) ^ 0xFF
-
-// 	// printBin8(magic)
-// 	fmt.Println("a, b, magic:", a, b, magic)
 // 	return (mask|1)*(MaxInt8-mask)-abs8(a) > (abs8(b) & int8(magic))
 // }
