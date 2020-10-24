66b79796e558d01646ee2a7d7ee40e2a305fcc7a
hjcian <chutarojian@gmail.com>
2020-10-24 16:47:54 +0800

    🐝 Bug: Use for-loop to verify the [01]/[10] sequences but avoiding to use if-branching

--- interesting-problems/bitwise-ops-allones/allones.go
+++ interesting-problems/bitwise-ops-allones/allones.go
@@ -1,9 +1,21 @@
 package allones
 
 func isAllEventOnes(n int32) bool {
-	return (n ^ int32(0b01010101010101010101010101010101)) == 0
+	verifier := int32(0b01)
+	mask := int32(0b11)
+	flag := int32(0b00)
+	expect := int32(0b00)
+	for i := 0; i < 16; i++ {
+		res := (n >> (2 * i)) & mask
+		flag |= (res ^ verifier)
+	}
+	return expect == flag
 }
 
 func isAllOddOnes(n int32) bool {
-	return (n ^ ^int32(0b01010101010101010101010101010101)) == 0
+	flag := int32(0b00)
+	for i := 0; i < 16; i++ {
+		flag |= ((n>>(2*i))&int32(0b11) ^ int32(0b10))
+	}
+	return int32(0b00) == flag
 }
--- interesting-problems/bitwise-ops-allones/allones_test.go
+++ interesting-problems/bitwise-ops-allones/allones_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func Test_allEvenOnes(t *testing.T) {
 	t.Run("allOddOnes", func(t *testing.T) {
+		// t.Skip()
 		testCases := []struct {
 			n    int32
 			want bool
@@ -13,6 +14,8 @@ func Test_allEvenOnes(t *testing.T) {
 			{^int32(0b01010101011101110101010111010101), false},
 			{int32(0b01010101011101110101010111010101), false},
 			{int32(0b01110101011101110101010111010101), false},
+			{int32(0b01110111011101110101010111010101), false},
+			{int32(0b01110100011101110101010111010101), false},
 		}
 		for _, test := range testCases {
 			if got := isAllOddOnes(test.n); got != test.want {
@@ -26,6 +29,7 @@ func Test_allEvenOnes(t *testing.T) {
 			want bool
 		}{
 			{int32(0b01010101010101010101010101010101), true},
+			{int32(0b01010101010101010101010101000101), false},
 			{^int32(0b01010101010101010101010101010101), false},
 			{int32(0b01010101010101010101010111010101), false},
 			{int32(0b01010101011101110101010111010101), false},
