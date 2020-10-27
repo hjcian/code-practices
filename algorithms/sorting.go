8ac29c453059c0dd1dd47b9e8d90ff9667d41b34
hjcian <chutarojian@gmail.com>
2020-10-27 20:45:57 +0800

    📚 Docs: Add comment in bubble sort

--- algorithms/sorting.go
+++ algorithms/sorting.go
@@ -34,12 +34,16 @@ func insertionSort(nums []int) []int {
 
 func bubbleSort(nums []int) []int {
 	end := len(nums)
+	// if end == 1，表示只剩下一個元素可排序 = 不用排序
 	for end > 1 {
 		for i := 0; i < end-1; i++ {
+			// 每次兩兩比較從頭看到尾，可確保最大的元素會被丟到後面
 			if nums[i] > nums[i+1] {
+				// 發現左邊比右邊大，swap them
 				nums[i], nums[i+1] = nums[i+1], nums[i]
 			}
 		}
+		// 故當我已經知道最大的元素被丟到後面，那麼下一次整理時，就可以不用看到最後一個
 		end--
 	}
 	return nums
